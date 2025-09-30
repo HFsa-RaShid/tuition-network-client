88995a26a0753697a0600914d3b471efb475a334
Hafsa_Rashid <hafsa20cse028@gmail.com>
2025-10-01 01:58:35 +0600

    d

--- src/pages/landingPage/Auth/SignIn/SignIn.jsx
+++ src/pages/landingPage/Auth/SignIn/SignIn.jsx
@@ -52,12 +52,12 @@ const SignIn = () => {
       <Helmet>
         <title>Sign_In | TuiToria</title>
       </Helmet>
-      <div className="flex flex-col md:flex-row w-4/5 max-w-5xl  my-2 overflow-hidden gap-20 p-6">
+      <div className="flex flex-col lg:flex-row w-4/5 max-w-5xl  my-2 overflow-hidden gap-8 lg:gap-20 p-6">
         {/* Left Image Section */}
-        <img src={signInImage} className="w-full md:w-1/2 h-80 mt-6 object-contain" />
+        <img src={signInImage} className="w-full lg:w-1/2 h-80 mt-6 object-contain" />
 
         {/* Right Form Section */}
-        <div className="w-full md:w-1/2 px-14 py-6 bg-white">
+        <div className="w-full lg:w-1/2 px-14 py-6 bg-white">
           <h2 className="text-2xl font-semibold text-gray-700 text-center">
             Welcome <span className="text-[#123d7e]">Back</span>
           </h2>
