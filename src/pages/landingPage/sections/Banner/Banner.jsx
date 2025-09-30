c5dd7b73a25881e2a44d9ff5a3822a42f6eb3891
Hafsa_Rashid <hafsa20cse028@gmail.com>
2025-09-30 21:48:04 +0600

    update

--- src/pages/landingPage/sections/Banner/Banner.jsx
+++ src/pages/landingPage/sections/Banner/Banner.jsx
@@ -1,6 +1,5 @@
 // import React, { useState } from "react";
 // import { useNavigate } from "react-router-dom";
-
 // import banner from "../../../../assets/teachers.png";
 // import yellowCircle from "../../../../assets/yellowCircle.png";
 // import grayCircle from "../../../../assets/grayCircle.png";
@@ -8,9 +7,9 @@
 // import yellowShape from "../../../../assets/yellowShape.png";
 // import blueShape from "../../../../assets/blueShape.png";
 // import wireShape from "../../../../assets/Highlight 26.png";
-
 // import bdDistricts from "../../../utils/bdDistricts";
 // import cityAreaMap from "../../../utils/cityAreaMap";
+
 
 // const Banner = () => {
 //   const [className, setClassName] = useState("");
@@ -211,7 +210,6 @@ import blueCircle from "../../../../assets/blueCircle.png";
 import yellowShape from "../../../../assets/yellowShape.png";
 import blueShape from "../../../../assets/blueShape.png";
 import wireShape from "../../../../assets/Highlight 26.png";
-
 import bdDistricts from "../../../utils/bdDistricts";
 import cityAreaMap from "../../../utils/cityAreaMap";
 
@@ -237,22 +235,21 @@ const Banner = () => {
   ];
 
   return (
-    <div className="bg-base-100 py-12 mt-20">
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col-reverse lg:flex-row items-center lg:gap-20 gap-10 px-20">
+    <div className="bg-base-100 py-16 mt-20">
+      <div className="container mx-auto">
+        <div className="flex flex-col-reverse md:flex-col-reverse lg:flex-row gap-10 lg:gap-32 justify-between px-6 lg:px-20">
           {/* ---------- LEFT SIDE ---------- */}
-          <div className="w-full lg:w-1/2 text-center lg:text-left">
-            <p className="text-[#DAA520] font-bold text-sm sm:text-base">
+          <div className="w-full lg:w-[55%] mt-8">
+            <p className="text-[#DAA520] font-bold">
               100% SATISFACTION GUARANTEE
             </p>
-
             <div className="my-4 font-bold">
-              <h1 className="text-3xl sm:text-4xl lg:text-5xl">Find Your</h1>
-              <h1 className="text-3xl sm:text-4xl lg:text-5xl py-4">
+              <h1 className="text-4xl md:text-5xl">Find Your</h1>
+              <h1 className="text-4xl md:text-5xl py-4">
                 Perfect{" "}
                 <span className="inline-flex items-center relative">
                   Tutor
-                  <span className="absolute bottom-0 top-6 left-0 w-full hidden sm:block">
+                  <span className="absolute bottom-0 top-6 left-0 w-full">
                     <svg
                       width="120"
                       height="57"
@@ -268,25 +265,40 @@ const Banner = () => {
                       />
                     </svg>
                   </span>
+                  <span className="absolute bottom-8 left-[118px] w-full">
+                    <svg
+                      width="40"
+                      height="35"
+                      viewBox="0 0 256 222"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M9.57951 166.787C6.95283 123.003 5.72063 35.4108 21.8054 35.3182C41.9113 35.2025 66.7846 121.96 64.7991 143.575C62.8136 165.191 88.6879 28.8063 117.55 10.0993C146.412 -8.60783 119.285 134.878 109.071 151.382C98.8577 167.885 210.559 83.2433 243.267 94.502C275.974 105.761 132.489 236.051 69.469 210.295"
+                        stroke="#0065ff"
+                        strokeWidth="15"
+                        strokeLinecap="round"
+                      />
+                    </svg>
+                  </span>
                 </span>
               </h1>
             </div>
 
-            <p className="font-medium text-gray-500 text-sm sm:text-base">
+            <p className="font-medium text-gray-500">
               Personalized one-on-one instruction tailored to your needs,
               delivered by a highly qualified instructor of your choice.
               Sessions can be conducted online or in person.
             </p>
 
             {/* ---------- SEARCH FORM ---------- */}
-            <div className="mt-8 flex flex-wrap justify-center lg:justify-start gap-4">
+            <div className="mt-10 flex flex-wrap gap-4">
               {/* Class Input */}
               <select
                 value={className}
                 onChange={(e) => setClassName(e.target.value)}
-                className={`border-b-2 border-gray-300 appearance-none focus:outline-none px-2 py-1 w-32 sm:w-36 ${
-                  !className ? "text-gray-400" : "text-black"
-                }`}
+                className={`border-b-2 border-gray-300 appearance-none focus:outline-none px-2 py-1 w-32 md:w-40 
+                ${!className ? "text-gray-400" : "text-black"}`}
               >
                 <option value="" disabled>
                   Class
@@ -305,9 +317,8 @@ const Banner = () => {
                   setDistrict(e.target.value);
                   setCity("");
                 }}
-                className={`border-b-2 border-gray-300 appearance-none focus:outline-none px-2 py-1 w-32 sm:w-36 ${
-                  !district ? "text-gray-400" : "text-black"
-                }`}
+                className={`border-b-2 border-gray-300 appearance-none focus:outline-none px-2 py-1 w-32 md:w-40
+                ${!district ? "text-gray-400" : "text-black"}`}
               >
                 <option value="" disabled>
                   District
@@ -324,9 +335,8 @@ const Banner = () => {
                 value={city}
                 onChange={(e) => setCity(e.target.value)}
                 disabled={!district}
-                className={`border-b-2 border-gray-300 appearance-none focus:outline-none px-2 py-1 w-32 sm:w-36 ${
-                  !city ? "text-gray-400" : "text-black"
-                }`}
+                className={`border-b-2 border-gray-300 appearance-none focus:outline-none px-2 py-1 w-32 md:w-40 
+                ${!city ? "text-gray-400" : "text-black"}`}
               >
                 <option value="" disabled>
                   Location
@@ -342,7 +352,7 @@ const Banner = () => {
               {/* Search Button */}
               <button
                 onClick={handleSearch}
-                className="bg-blue-200 text-blue-700 px-4 py-2 rounded hover:bg-blue-300 shadow"
+                className="bg-blue-200 text-blue-700 px-3 py-2 rounded hover:bg-blue-300 shadow"
               >
                 Search
               </button>
@@ -350,36 +360,36 @@ const Banner = () => {
           </div>
 
           {/* ---------- RIGHT SIDE IMAGE ---------- */}
-          <div className="w-full lg:w-1/2 flex justify-center lg:justify-end">
-            <div className="relative max-w-[320px] sm:max-w-[400px] w-full">
+          <div className="w-full lg:w-[45%] flex justify-center lg:justify-end">
+            <div className="relative w-[280px] md:w-[350px] lg:w-[400px]">
               <img
                 src={banner}
                 alt="Banner"
-                className="w-full h-auto relative z-10"
+                className="h-[280px] md:h-[350px] lg:h-[400px] relative z-10 w-full"
               />
               <img
                 src={yellowCircle}
-                className="absolute top-[-30px] -right-12 z-0 opacity-50 w-16 sm:w-20"
+                className="absolute top-[-30px] -right-12 z-0 opacity-50"
               />
               <img
                 src={blueShape}
-                className="absolute top-[-20px] -right-16 z-0 h-[30px] sm:h-[40px]"
+                className="absolute top-[-20px] -right-16 z-0 h-[40px]"
               />
               <img
                 src={grayCircle}
-                className="absolute top-[120px] -right-16 z-0 opacity-50 w-16 sm:w-20"
+                className="absolute top-[150px] -right-20 z-0 opacity-50"
               />
               <img
                 src={yellowShape}
-                className="absolute top-[100px] -right-12 z-0 h-[30px] sm:h-[40px]"
+                className="absolute top-[132px] -right-16 z-0 h-[40px]"
               />
               <img
                 src={blueCircle}
-                className="absolute bottom-[-20px] -left-6 z-0 opacity-50 w-16 sm:w-20"
+                className="absolute bottom-[-30px] -left-8 z-0 opacity-50"
               />
               <img
                 src={wireShape}
-                className="absolute bottom-0 -left-12 z-0 h-[15px] sm:h-[20px]"
+                className="absolute bottom-0 -left-16 z-0 h-[20px]"
               />
             </div>
           </div>
--- src/pages/landingPage/sections/countDown/CountDown.jsx
+++ src/pages/landingPage/sections/countDown/CountDown.jsx
@@ -1,30 +1,55 @@
+// import React from "react";
+// const CountDown = () => {
+//   return (
+//     <div className=" h-[130px] w-full">
+
+//         <div className="h-full bg-gray-500 flex items-center justify-around text-white container mx-auto">
+//         <div>
+//           <h1 className="text-3xl">5000</h1>
+//           <p>Verified Tutors</p>
+//         </div>
+//         <div className="h-[70%] border border-gray-300"></div>
+//         <div>
+//           <h1 className="text-3xl">15000</h1>
+//           <p>Available Tuitions</p>
+//         </div>
+//         <div className="h-[70%] border border-gray-300"></div>
+
+//         <div>
+//           <h1 className="text-3xl">5000</h1>
+//           <p>Matched Tuitions</p>
+//         </div>
+//       </div>
+//       </div>
+ 
+//   );
+// };
+
+// export default CountDown;
+
+
 import React from "react";
+
 const CountDown = () => {
   return (
-    <div className=" h-[130px] w-full">
-
-        <div className="h-full bg-gray-500 flex items-center justify-around text-white container mx-auto">
+    <div className="h-[130px] w-full bg-gray-500 text-white flex items-center">
+      <div className="container mx-auto flex items-center justify-around">
         <div>
           <h1 className="text-3xl">5000</h1>
           <p>Verified Tutors</p>
         </div>
         <div className="h-[70%] border border-gray-300"></div>
-
-
         <div>
           <h1 className="text-3xl">15000</h1>
           <p>Available Tuitions</p>
         </div>
         <div className="h-[70%] border border-gray-300"></div>
-
-
         <div>
           <h1 className="text-3xl">5000</h1>
           <p>Matched Tuitions</p>
         </div>
       </div>
-      </div>
- 
+    </div>
   );
 };
 
